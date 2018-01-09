308297c96db80d94ed5adcc568816a632a4b7fbd
Philip Kuryloski <pkuryloski@pivotal.io>
2018-01-09 13:15:48 +0100

    Revert "Enable less-disk"
    This reverts commit 8db571dd76432c24047c3b7c23cdce42097fcf99.

--- update-cloud-config.sh
+++ update-cloud-config.sh
@@ -7,7 +7,6 @@ set -euxo pipefail
 bosh -n update-cloud-config cloud-config.yml \
   -o two-workers.yml \
   -o smaller-vms.yml \
-  -o less-disk.yml \
   -o adjust-networks.yml \
   -o disable-external-ips.yml \
   -o cf.yml
