c304ab3752038fdfcabf3bc4ec9ef011d83291df
Philip Kuryloski <pkuryloski@pivotal.io>
2018-01-10 17:03:39 +0100

    Quote the JSON in the shell

--- terraform-import.sh
+++ terraform-import.sh
@@ -5,6 +5,6 @@ set -euxo pipefail
 terraform import \
   -var project_id=${PROJECT_ID} \
   -var region=${REGION} \
-  -var credentials=${GOOGLE_CREDENTIALS} \
+  -var credentials="${GOOGLE_CREDENTIALS}" \
   google_compute_network.bbl-network
   ${NETWORK_NAME}
