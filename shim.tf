30d345f77f0ab4b52f093d37f5495342ef6cedb0
Philip Kuryloski <pkuryloski@pivotal.io>
2018-01-11 08:43:24 +0100

    Protect the gcp network created by bbl

--- shim.tf
+++ shim.tf
@@ -5,5 +5,7 @@ variable "env_id" {
 provider "google" {}
 
 resource "google_compute_network" "bbl-network" {
-  name = "${var.env_id}-network"
+  name                    = "${var.env_id}-network"
+  auto_create_subnetworks = false
+  prevent_destroy         = true
 }
