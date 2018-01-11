cceef564be5b8d29532802a9bc838423b135fc23
Philip Kuryloski <pkuryloski@pivotal.io>
2018-01-11 12:57:32 +0100

    Add terraform init call

--- terraform-apply.sh
+++ terraform-apply.sh
@@ -5,6 +5,8 @@ set -euxo pipefail
 cp bosh-bosh/shim.tf bosh-bosh/template.tf .
 cp terraform-in/terraform.tfstate .
 
+terraform init
+
 terraform plan \
   -var env_id=${ENV_ID} \
   -var ssl_certificate="${CERTIFICATE}" \
