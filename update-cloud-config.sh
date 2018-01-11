cb4035a2ee52c687cf2fa3a85aeb9460f305f1c4
Philip Kuryloski <pkuryloski@pivotal.io>
2018-01-11 16:45:52 +0100

    start reading terraform when updating cloud config

--- update-cloud-config.sh
+++ update-cloud-config.sh
@@ -4,6 +4,13 @@ cd "$(dirname "$0")"
 
 set -euxo pipefail
 
+# cp bosh-bosh/shim.tf bosh-bosh/template.tf .
+cp terraform-in/terraform.tfstate .
+
+# terraform init
+
+terraform output
+
 bosh -n update-cloud-config cloud-config.yml \
   -o two-workers.yml \
   -o smaller-vms.yml \
