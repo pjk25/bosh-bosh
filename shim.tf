6be35ccf5d284bd94d917387927a98f7bf9299c8
Philip Kuryloski <pkuryloski@pivotal.io>
2018-01-11 08:57:18 +0100

    Fixup

--- shim.tf
+++ shim.tf
@@ -7,5 +7,8 @@ provider "google" {}
 resource "google_compute_network" "bbl-network" {
   name                    = "${var.env_id}-network"
   auto_create_subnetworks = false
-  prevent_destroy         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
