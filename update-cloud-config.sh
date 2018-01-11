90a057c7f617525882dfd82c21a57754d878ed09
Philip Kuryloski <pkuryloski@pivotal.io>
2018-01-11 17:13:39 +0100

    Fix working directory

--- update-cloud-config.sh
+++ update-cloud-config.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "$0")"
-
 set -euxo pipefail
+
+export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ls -al
 
@@ -13,9 +13,9 @@ cp terraform-in/terraform.tfstate .
 
 terraform output
 
-bosh -n update-cloud-config cloud-config.yml \
-  -o two-workers.yml \
-  -o smaller-vms.yml \
-  -o adjust-networks.yml \
-  -o disable-external-ips.yml \
-  -o cf.yml
+bosh -n update-cloud-config ${SCRIPT_DIR}/cloud-config.yml \
+  -o ${SCRIPT_DIR}/two-workers.yml \
+  -o ${SCRIPT_DIR}/smaller-vms.yml \
+  -o ${SCRIPT_DIR}/adjust-networks.yml \
+  -o ${SCRIPT_DIR}/disable-external-ips.yml \
+  -o ${SCRIPT_DIR}/cf.yml
