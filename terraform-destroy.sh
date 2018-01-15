7f68256162d9b31b853e6846325d23bc55017344
Philip Kuryloski <pkuryloski@pivotal.io>
2018-01-15 11:24:45 +0100

    update targets

--- terraform-destroy.sh
+++ terraform-destroy.sh
@@ -11,24 +11,36 @@ terraform destroy \
   -var env_id=${ENV_ID} \
   -var ssl_certificate="${CERTIFICATE}" \
   -var ssl_certificate_private_key="${PRIVATE_KEY}" \
-  -target firewall-cf \
-  -target cf-address \
-  -target cf-http-forwarding-rule \
-  -target cf-https-forwarding-rule \
-  -target cf-http-lb-proxy \
-  -target cf-https-lb-proxy \
-  -target cf-cert \
-  -target cf-https-lb-url-map \
-  -target cf-public-health-check \
-  -target cf-health-check \
-  -target cf-ssh-proxy \
-  -target cf-tcp-router \
-  -target cf-ws \
-  -target cf-ws-https \
-  -target cf-ws-http \
-  -target credhub \
-  -target router-lb-0 \
-  -target router-lb-1 \
-  -target router-lb-2 \
-  -target router-lb-backend-service \
+  -target google_compute_firewall.firewall-cf \
+  -target google_compute_global_address.cf-address \
+  -target google_compute_global_forwarding_rule.cf-http-forwarding-rule \
+  -target google_compute_global_forwarding_rule.cf-https-forwarding-rule \
+  -target google_compute_target_http_proxy.cf-http-lb-proxy \
+  -target google_compute_target_https_proxy.cf-https-lb-proxy \
+  -target google_compute_ssl_certificate.cf-cert \
+  -target google_compute_url_map.cf-https-lb-url-map \
+  -target google_compute_health_check.cf-public-health-check \
+  -target google_compute_http_health_check.cf-public-health-check \
+  -target google_compute_firewall.cf-health-check \
+  -target google_compute_address.cf-ssh-proxy \
+  -target google_compute_firewall.cf-ssh-proxy \
+  -target google_compute_target_pool.cf-ssh-proxy \
+  -target google_compute_forwarding_rule.cf-ssh-proxy \
+  -target google_compute_firewall.cf-tcp-router \
+  -target google_compute_address.cf-tcp-router \
+  -target google_compute_http_health_check.cf-http-router \
+  -target google_compute_target_pool.cf-http-router \
+  -target google_compute_forwarding_rule.cf-http-router \
+  -target google_compute_address.cf-ws \
+  -target google_compute_target_pool.cf-ws \
+  -target google_compute_forwarding_rule.cf-ws-https \
+  -target google_compute_forwarding_rule.cf-ws-http \
+  -target google_compute_firewall.credhub \
+  -target google_compute_address.credhub \
+  -target google_compute_target_pool.credhub \
+  -target google_compute_forwarding_rule.credhub \
+  -target google_compute_instance_group.router-lb-0 \
+  -target google_compute_instance_group.router-lb-1 \
+  -target google_compute_instance_group.router-lb-2 \
+  -target google_compute_backend_service.router-lb-backend-service \
   -state-out terraform-out/terraform.tfstate
