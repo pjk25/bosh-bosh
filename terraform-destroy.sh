9be195cc437a2be39ff734e237ae102f0a13e098
Philip Kuryloski <pkuryloski@pivotal.io>
2018-01-15 10:41:09 +0100

    Add destroy script

---
+++ terraform-destroy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+set -euxo pipefail
+
+cp bosh-bosh/shim.tf bosh-bosh/template.tf .
+cp terraform-in/terraform.tfstate .
+
+terraform init
+
+terraform destroy \
+  -var env_id=${ENV_ID} \
+  -var ssl_certificate="${CERTIFICATE}" \
+  -var ssl_certificate_private_key="${PRIVATE_KEY}" \
+  -state-out terraform-out/terraform.tfstate
